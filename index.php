9af38983d0a260e09b2cdd24ad54e1995fb85f98
Bryan Isaac Lucatero Cervantes <126211404+Bryan010719@users.noreply.github.com>
2024-06-13 12:35:25 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,445 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="imagenes/icono.jpeg">
+    <title>PARCEL</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles/brands.css">
+    <link rel="stylesheet" href="styles/fontawesome.css">
+    <link rel="stylesheet" href="styles/solid.css">
+    <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
+    <link rel="stylesheet" href="estiloperro.css">
+    />
+    <!-- <style>
+      .animate__animated.animate__wobble{
+  --animate-duration: 20s;
+}
+    </style> -->
+    
+    
+  </head>
+<body>
+    
+  
+  
+
+    <!-- menu -->
+    <nav class="navbar fixed-top navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">PARCEL CHEWEY <i class="fa-solid fa-teeth-open"></i></a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">  
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Options
+                </a>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="#carouselExampleCaptions">Home</a></li>
+                  <li><a class="dropdown-item" href="#targetas-info">articles</a></li>
+                  
+                  <li><a class="dropdown-item" href="#registro-datos">Register data</a></li>
+                </ul>
+              </li>
+              
+            </ul>
+            
+          </div>
+        </div>
+      </nav>
+
+    
+      <!-- slide show -->
+      
+      <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel" data-bs-interval="5000" >
+        <div class="carousel-indicators">
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+          
+        </div>
+        <div class="carousel-inner">
+          <div class="carousel-item active" style="height: 500px;">
+            <img src="imagenes/po1.jpeg" class="d-block w-100" alt="imagen" height="500px" width="auto">
+            <div class="carousel-caption d-none d-md-block">
+              <h5><b>We are the best</b></h5>
+              <p></p>
+            </div>
+          </div>
+          <div class="carousel-item" style="height: 500px;">
+            <img src="imagenes/po2.jpeg" class="d-block w-100" alt="imagen"height="500px" width="auto">
+            <div class="carousel-caption d-none d-md-block">
+              <h5><b>We diliver</b></h5>
+              <p></p>
+            </div>
+          </div>
+          <div class="carousel-item" style="height: 500px;">
+            <img src="imagenes/po4.jpeg" class="d-block w-100" alt="imagen"height="500px" width="auto">
+            <div class="carousel-caption d-none d-md-block">
+              <h5><b>We are your best choice</b></h5>
+              <p></p>
+            </div>
+          </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+
+
+
+      <!--  -->
+      <!--  -->
+   
+   
+   
+   
+   
+   
+   
+      <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+
+            </div>
+        </div>
+
+    </div>
+    <div class="container-fluid" style="padding-top: 30px;">
+        <div class="row">
+            <div class="col-md-12">
+
+            </div>
+        </div>
+
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="brands.js"></script>
+    <script src="fontawesome.js"></script>
+    <script src="solid.js"></script>
+<!--  -->
+<br>
+<br>
+<br>
+<!--  -->
+
+    <!-- <div class="container" style="margin-top: 100px; background-image: url(imagenes/lackers.png);
+             background-attachment: fixed;
+              background-position: center;
+              background-repeat: no-repeat;
+              background-size: cover;" > -->
+      <div class="row">
+        <div class="col-md-12 animate__animated animate__wobble"  >
+
+          <div class="d-flex flex-row"id="targetas-info">
+            <div class="card mx-auto"> <div class="card h-100" style="width: 18rem;">
+              <img src="imagenes/po1.jpeg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Land shipping</h5>
+                <p class="card-text">Dilevery</p>
+
+
+
+
+              
+                  <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#lebron1" aria-expanded="false" aria-controls="collapseExample">
+                    INFO
+                  </button>
+                </p>
+                <div class="collapse" id="lebron1">
+                  <div class="card card-body">
+                  Diliver on land
+                  </div>
+                </div>
+
+
+                <a id="perro-culaso" href="https://www.savinodelbene.com/land-shipments-characteristics-and-different-types/"class="btn btn-primary">Article about land shipping</a>
+              </div>
+            </div>
+  </div>
+
+            <div class="card mx-auto"> <div class="card h-100" style="width: 18rem;">
+              <img src="imagenes/po2.jpeg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Air shipping</h5>
+                <p class="card-text">Delivery</p>
+
+                <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#lebron2" aria-expanded="false" aria-controls="collapseExample">
+                  INFO
+                </button>
+              </p>
+              <div class="collapse" id="lebron2">
+                <div class="card card-body">
+                Air Shipping: Refers to all goods transported by air, including luggage on passenger flights, parcels sent through express services, and large shipments in cargo planes.
+                </div>
+              </div>
+
+
+
+
+                
+                <a href="https://www.maersk.com/logistics-explained/transportation-and-freight/2024/04/04/choosing-air-freight" class="btn btn-primary">Article about air shipping</a>
+              </div>
+            </div>
+  </div>
+
+            <div class="card mx-auto"> <div class="card h-100" style="width: 18rem;">
+              <img src="imagenes/po4.jpeg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Ocean shipping</h5>
+                <p class="card-text">Dilevery</p>
+
+
+                <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#lebron3" aria-expanded="false" aria-controls="collapseExample">
+                  INFO
+                </button>
+              </p>
+              <div class="collapse" id="lebron3">
+                <div class="card card-body">
+                Ocean freight shipping is the method of transporting containerised cargo loaded onto vessels by sea. Over 90% of all the world’s trade is carried by sea. If you want to ship your freight around the world, ocean freight is the most convenient option.
+                </div>
+              </div>
+
+
+
+
+                <a href="https://www.maersk.com/transportation-services/ocean-transport?gad_source=1&gclid=CjwKCAjw65-zBhBkEiwAjrqRMImmCKYm4PSV3xY2f1pZs0RAREn20PZ_p_KvVYH23BNyKaRY8VK4GhoCN4oQAvD_BwE&gclsrc=aw.ds" class="btn btn-primary">Article about ocean shipping</a>
+              </div>
+            </div>
+  </div>
+          </div>
+
+
+
+          
+          
+        </div>
+      </div>
+    </div>
+    
+<!-- Button trigger modal -->
+ <br>
+ <br>
+ <br>
+ <br>
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+  <b>Types of shipping</b>
+</button> 
+<br>
+<br>
+<hr>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel"><b>VIDEO:</b></h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <iframe width="460" height="215" src="https://youtu.be/jKwHvXEDXMs?si=YMjYEN6-spSbtjLD" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <!-- <button type="button" class="btn btn-primary">Close</button> -->
+      </div>
+    </div>
+  </div>
+</div>
+<br>
+  <div class="registro-de-datos" id="registro-datos">
+  <h1>REGISTER THE DATA:</h1>
+  </div>
+          
+          
+  </body>
+  
+</html>
+<?php  require_once('funciones.php')?>
+<!DOCTYPE html>
+<head>
+    <link rel="stylesheet" href="formgroup.css">      
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+</head>
+<body>
+<?php
+
+
+// conexion a la base de datos
+$servername = "p:localhost";
+$databasename ="tienda";
+$username = "root";
+$password = "";
+$con = mysqli_connect($servername, $username, $password,$databasename);
+mysqli_set_charset($con, "utf8");
+
+//consulta a la base de datos
+$query_DatosConsulta =  sprintf("SELECT * FROM almacen");
+$DatosConsulta = mysqli_query($con, $query_DatosConsulta) or die(mysqli_error($con));
+$row_DatosConsulta =mysqli_fetch_assoc($DatosConsulta);
+$totalRows_DatosConsulta = mysqli_num_rows($DatosConsulta);
+
+//mostrar la informacion de la tabla autor
+if($totalRows_DatosConsulta > 0 )
+{
+    do{
+        echo("<hr>"); echo $row_DatosConsulta["NA"]; echo("   ");
+        echo $row_DatosConsulta["NombreProducto"];echo("   ");
+        echo $row_DatosConsulta["LugarImporte"];echo("   ");
+        echo $row_DatosConsulta["LugarExporte"];echo("   ");
+        echo "<br>";
+    
+    }while($row_DatosConsulta = mysqli_fetch_assoc($DatosConsulta));
+    
+//if de insertar informacon
+}
+if((isset($_POST["MM_insert"])) && ($_POST["MM_insert"] == "formInsertar")){
+    $insertSQL = sprintf("INSERT INTO almacen(NombreProducto, LugarImporte, LugarExporte) Values (%s,%s,%s)",
+    GetSQLValueString($_POST["strNombreProducto"], "text"),
+    GetSQLValueString($_POST["strLugarImporte"], "text"),
+    GetSQLValueString($_POST["strLugarExporte"], "text"));
+    $Result1 = mysqli_query($con, $insertSQL) or die(mysqli_error($con));
+    $insertGoTo = "/php/index.php";
+    (sprintf("Location: %s", $insertGoTo));
+
+}
+//if de borrar informacion
+
+if ((isset($_POST["MM_delete"])) && ($_POST["MM_delete"] == "fromBorrar")) {
+    $query_Delete = sprintf("DELETE FROM almacen WHERE NA=%s",
+    GetSQLValueString($_POST["intNA"],"int"));
+    $Result1 = mysqli_query($con, $query_Delete) or die(mysqli_error($con));
+    $insertGoTo = "/php/index.php";
+    (sprintf("Location: %s", $insertGoTo));
+  }
+
+
+
+
+
+
+
+
+
+?>
+<!-- formulario de insertar informacion -->
+<form action="/php/index.php" method="post" id="formInsertar" role="form" name="formInsertar">
+    <div class= "form-group">
+        <label >Product name:</label>
+        <input name="strNombreProducto" class="form-control" id="strNombreProducto" placeholder="Product name">
+
+    </div>
+    <div class= "form-group">
+        <label >Imported from:</label>
+        <input name="strLugarImporte" class="form-control" id="strLugarImporte" placeholder="Place where it was imported form">
+
+    </div>
+    <div class= "form-group">
+        <label >Exported from:</label>
+        <input name="strLugarExporte" class="form-control" id="strLugarExporte" placeholder="Place where it was exported from">
+
+    </div>
+  
+    <input name="MM_insert" type="hidden" id="MM_insert" value="formInsertar">
+    <button type="submit" class="btnbtn-succes">Add</button>
+
+    </form>
+
+
+    <!-- formulario de borrar informacion -->
+    <form action= "/php/index.php" method="post" id="fromBorrar" role="form" name="fromBorrar">
+    <div class= "form-group">
+        <label >NA:</label>
+        <input name="intNA" class="form-control" id="intNA" placeholder="Place NA value">
+    </div>
+   
+     <input name="MM_delete" type="hidden" id="MM_delete" value="fromBorrar">
+     <button type="submit" class="btn-btn-success">Delete</button>
+   </form>
+
+
+   
+
+
+    <!-- formulario de actualizar informacion -->
+
+  <form action="/php/index.php" method="post" id="formActualizar" role="form" name="formActualizar">
+    <div class= "form-group">
+        <label >Product name:</label>
+        <input name="strNombreProducto" class="form-control" id="strNombreProducto" placeholder="Change name of product" >
+    </div>
+    <div class= "form-group">
+        <label >Imported from:</label>
+        <input name="strLugarImporte" class="form-control" id="strLugarImporte" placeholder="Change place where it was exported from">
+    </div>
+    <div class= "form-group">
+        <label >Exported from:</label>
+        <input name="strLugarExporte" class="form-control" id="strLugarExporte" placeholder="Change place where it was imported from" <?php ?>>
+    </div>
+    <div class= "form-group">
+        <label >NA:</label>
+        <input name="intNA" class="form-control" id="intNA" placeholder="Place NA value">
+    </div>
+    <input name="MM_Actualizar" type="hidden" id="MM_Actualizar" value="formActualizar">
+    <button type="submit" class="btnbtn-succes">Update info</button>
+</form>
+
+<?php
+// $query_DatosConsultau =  sprintf("SELECT * FROM autor WHERE NA=46");
+// $DatosConsultau = mysqli_query($con, $query_DatosConsultau) or die(mysqli_error($con));
+// $row_DatosConsultau =mysqli_fetch_assoc($DatosConsultau);
+// $totalRows_DatosConsultau = mysqli_num_rows($DatosConsultau);
+
+
+
+
+
+
+// if de actualizar informacion
+if ((isset($_POST["MM_Actualizar"])) && ($_POST["MM_Actualizar"] == "formActualizar")) {
+    
+    $query_Act = sprintf("UPDATE almacen SET NombreProducto=%s, LugarImporte=%s, LugarExporte=%s WHERE NA=%s",
+        GetSQLValueString($_POST["strNombreProducto"], "text"),
+        GetSQLValueString($_POST["strLugarImporte"], "text"),
+        GetSQLValueString($_POST["strLugarExporte"], "text"),
+        GetSQLValueString($_POST["intNA"], "int"));
+        // GetSQLValueString(2, "text"));
+    $Result1 = mysqli_query($con, $query_Act) or die (mysqli_error($con));
+    $insertGoTo = "/php/index.php";
+    (sprintf("Location: %s", $insertGoTo));
+}
+?>
+
+
+
+<?php
+mysqli_close($con);
+?>
+
+
+
+
+
+
+
+
+
+
+ </html>
